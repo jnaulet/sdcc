c603ce24c16193f63bb08f319129bf86df5824a5
Joachim Naulet <joachim.naulet.ext@maia-space.com>
2024-06-04 13:23:38 +0200

    s08-stack-auto: support for native _divulong

---
+++ sdcc/device/lib/s08-stack-auto/_divulong.c
@@ -0,0 +1,107 @@
+/*-------------------------------------------------------------------------
+   _divulong.c - routine for division of 32 bit unsigned long
+
+   Copyright (C) 1999, Jean-Louis Vern <jlvern AT gmail.com>
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 2, or (at your option) any
+   later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library; see the file COPYING. If not, write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+   As a special exception, if you link this library with other files,
+   some of which are compiled with SDCC, to produce an executable,
+   this library does not by itself cause the resulting executable to
+   be covered by the GNU General Public License. This exception does
+   not however invalidate any other reasons why the executable file
+   might be covered by the GNU General Public License.
+-------------------------------------------------------------------------*/
+static void
+_divulong_dummy (void) __naked
+{
+#define B 15 /* b: [15-18],s -> PARAM_2 */
+#define A 11 /* a: [11-14],s -> PARAM_1 */
+#define Q  5 /* q:   [5-8],s -> quotient */
+#define Z  1 /* z:   [1-4],s -> return */
+
+	__asm
+
+	.globl __divulong
+
+__divulong:
+	ais #-8
+	ldhx #0
+	;; q = 0
+	sthx (Q + 2),s
+	sthx (Q + 0),s
+	;; z = 0
+	sthx (Z + 2),s
+	sthx (Z + 0),s
+	;; n = 32 + 1
+	ldx #32 + 1
+.loop:
+	;; z <<= 1
+	asl (Z + 3),s
+	rol (Z + 2),s
+	rol (Z + 1),s
+	rol (Z + 0),s
+	;; compare q with PARAM_2
+	lda (Q + 3),s
+	sub (B + 3),s
+	lda (Q + 2),s
+	sbc (B + 2),s
+	lda (Q + 1),s
+	sbc (B + 1),s
+	lda (Q + 0),s
+	sbc (B + 0),s
+	bcs .next
+	;; if q >= PARAM_2, q-= PARAM_2
+	lda (Q + 3),s
+	sub (B + 3),s
+	sta (Q + 3),s
+	lda (Q + 2),s
+	sbc (B + 2),s
+	sta (Q + 2),s
+	lda (Q + 1),s
+	sbc (B + 1),s
+	sta (Q + 1),s
+	lda (Q + 0),s
+	sbc (B + 0),s
+	sta (Q + 0),s
+	inc (Z + 3),s ; z |= 1
+.next:
+	;; q:PARAM_1 <<= 1
+	;; lsl PARAM_1 + C
+	asl (A + 3)
+	rol (A + 2)
+	rol (A + 1)
+	rol (A + 0)
+	;; rol q
+	rol (Q + 3),s
+	rol (Q + 2),s
+	rol (Q + 1),s
+	rol (Q + 0),s
+	;; while n != 0
+	dbnzx .loop
+	;; return z
+	pula
+	sta *___SDCC_hc08_ret2
+	pula
+	sta *___SDCC_hc08_ret3
+	pulx
+	pula
+	;; redeem stack
+	ais #4
+	rts
+
+	__endasm;
+}
